807f1fc95e57ca1f11b1be107e8ba7ff89308026
Fernando Ribeira <github@fernandoribeira.com.br>
2022-02-11 22:35:25 -0300

    Estudando

--- Java COMPLETO Programação Orientada a Objetos +Projetos/Código/Seção 06 - Estruturas repetitivas/Aula 044/While/src/Main.java
+++ Java COMPLETO Programação Orientada a Objetos +Projetos/Código/Seção 06 - Estruturas repetitivas/Aula 044/While/src/Main.java
@@ -5,11 +5,15 @@ public class Main {
 
 		Scanner sc = new Scanner(System.in);
 		
+		int soma = 0;
 		int x = sc.nextInt();
 		
 		while (x != 0) {
+			soma += x;
 			x = sc.nextInt();
 		}
+		
+		System.out.println(soma);
 		
 		sc.close();
 		
