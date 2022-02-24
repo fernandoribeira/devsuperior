3d508a144432eee0e4d98cde4094c6d059718a0f
Fernando Ribeira <github@fernandoribeira.com.br>
2022-02-24 20:54:40 -0300

    Estudando

---
+++ Java COMPLETO Programação Orientada a Objetos +Projetos/Código/Seção 10 - Comportamento de memória, arrays, listas/Aula 093/ForEach/src/application/Program.java
@@ -0,0 +1,19 @@
+package application;
+
+public class Program {
+	public static void main(String[] args) {
+
+		String[] vect = new String[] {"Bolacha", "Minhoca", "Gabriel"};
+		
+		for (int i = 0; i < vect.length; i++) {
+			System.out.println(vect[i]);
+		}
+		
+		System.out.println();
+		
+		for (String nome : vect) {
+			System.out.println(nome);
+		}
+
+	}
+}
