7810ffbe20eb59b61bd0f7f18e7195263bb3a6b3
Fernando Ribeira <github@fernandoribeira.com.br>
2022-02-28 20:36:35 -0300

    Estudando

--- Java COMPLETO Programação Orientada a Objetos +Projetos/Código/Seção 11 - Tópicos especiais em Java/Aula 102/Datas/src/application/Program.java
+++ Java COMPLETO Programação Orientada a Objetos +Projetos/Código/Seção 11 - Tópicos especiais em Java/Aula 102/Datas/src/application/Program.java
@@ -2,7 +2,9 @@ package application;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.util.Date;
+import java.util.TimeZone;
 
 public class Program {
 	public static void main(String[] args) throws ParseException {
@@ -16,9 +18,37 @@ public class Program {
 		System.out.println(sdf1.format(y1));
 		System.out.println(sdf2.format(y2));
 		
-		Date d1 = new Date();
-		Date d2 = new Date(12346579);
-		Date d3 = new Date()
+		System.out.println("---------------------------");
+		System.out.println("Timezone BRT");
 		
+		SimpleDateFormat sdf3 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss z");
+
+		Date y3 = new Date();
+		Date y4 = new Date(1000L * 60L * 60L * 5L); // Milisegundos * minutos * horas * dias
+		Date y5 = new Date(System.currentTimeMillis());
+		Date y6 = Date.from(Instant.parse("2018-06-25T15:42:07Z"));
+		Date y7 = new Date(0L); 
+		
+		System.out.println(sdf3.format(y1));
+		System.out.println(sdf3.format(y2));
+		System.out.println(sdf3.format(y3));
+		System.out.println(sdf3.format(y4));
+		System.out.println(sdf3.format(y5));
+		System.out.println(sdf3.format(y6));
+		System.out.println(sdf3.format(y7));
+		
+		System.out.println("---------------------------");
+		System.out.println("Timezone GMT");
+		
+		sdf3.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
+		System.out.println(sdf3.format(y1));
+		System.out.println(sdf3.format(y2));
+		System.out.println(sdf3.format(y3));
+		System.out.println(sdf3.format(y4));
+		System.out.println(sdf3.format(y5));
+		System.out.println(sdf3.format(y6));
+		System.out.println(sdf3.format(y7));
+
 	}
 }
